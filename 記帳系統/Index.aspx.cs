5ea9fd63af992791607e8c85c8a1117c81a9837c
109370114@cc.ntut.edu.tw <109370114@cc.ntut.edu.tw>
2023-12-10 23:14:59 +0800

    20231210.v2更新

---
+++ 記帳系統/Index.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace 記帳系統
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MemberRegisterIndex.aspx");
+        }
+        
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("IncomeRegistrationIndex.aspx");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("IncomeReceiptIndex.aspx");
+        }
+    }
+}
